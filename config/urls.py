146e5c74f2e301e02343d2bfa02be79edd6bb7e3
Juan Paredes <juanmadpb@gmail.com>
2021-01-09 14:35:36 -0300

    Update users path.

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	# path('api/users/', include('apps.users.urls'))
-	path('', include('apps.users.urls'))
+	path('api/users/', include('apps.users.urls'))
 ]
