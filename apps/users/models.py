1e71e2f39f60a955367ef2bd00fd8cec8093a4ee
Juan Paredes <juanmadpb@gmail.com>
2021-01-09 14:36:54 -0300

    Update UserProfile class.

--- apps/users/models.py
+++ apps/users/models.py
@@ -44,9 +44,15 @@ class User(AbstractBaseUser):
 		"Does the user have a specific permission?"
 		return True
 
-class userProfile(models.Model):
+class UserProfile(models.Model):
 	user = models.OneToOneField(
 		User,
 		on_delete=models.CASCADE,
 		primary_key=True
 	)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	image = models.ImageField(upload_to='profile-images')
+
+	def __str__(self):
+		return f'{self.first_name} {self.last_name}'
