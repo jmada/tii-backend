68f82dbb4ad4cf5dd1309a00ad8f0f83bf554421
Juan Paredes <juanmadpb@gmail.com>
2021-01-09 14:37:41 -0300

    Add UserViewSet.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import serializers, viewsets
+from django.contrib.auth import get_user_model
 
-# Create your views here.
-def main(request):
-	return HttpResponse("Poop")
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
