a1b63ede5ba9d8fa705eb3bf0100f1c2b6082550
Juan Paredes <juanmadpb@gmail.com>
2021-01-11 18:50:45 -0300

    Add Admin Model.

--- apps/items/admin.py
+++ apps/items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('description', 'serial_number', 'image', 'created_at', 'updated_at')
+	search_fields = ('description',)
+
+admin.site.register(Item, ItemAdmin)
