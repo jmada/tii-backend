3e4f560343a19b1923e4462b04173aefe3d6f8e7
Juan Paredes <juanmadpb@gmail.com>
2021-01-11 18:51:07 -0300

    Add view.

--- apps/items/views.py
+++ apps/items/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+from .models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('id', 'description', 'created_at', 'updated_at',)
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
