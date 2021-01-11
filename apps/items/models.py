9f6bdf837afa018ba9d3f944e4fb7ca2997aa547
Juan Paredes <juanmadpb@gmail.com>
2021-01-11 18:50:53 -0300

    Add Model.

--- apps/items/models.py
+++ apps/items/models.py
@@ -4,5 +4,5 @@ from apps.utils.models import Timestamps
 
 class Item(Timestamps, models.Model):
 	description = models.CharField(max_length=100)
-	serial_number = models.CharField(max_length=100)
-	image = models.ImageField(upload_to='item-images')
+	serial_number = models.CharField(max_length=100, blank=True, default='')
+	image = models.ImageField(upload_to='item-images', blank=True)
